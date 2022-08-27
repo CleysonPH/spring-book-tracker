1b7f2323d13a7c36d7b8d84fb2ff9f3a751ff7c2
Cleyson Lima <cleysonph@gmail.com>
2022-08-27 02:37:10 -0300

    implemente the update author by id route

--- src/main/java/dev/cleysonph/booktracker/api/v1/author/routes/AuthorRoutes.java
+++ src/main/java/dev/cleysonph/booktracker/api/v1/author/routes/AuthorRoutes.java
@@ -7,9 +7,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AuthorRoutes {
 
-    public static final String CREATE_AUTHOR_ROUTE = ApiV1Routes.BASE_ROUTE + "/authors";
-    public static final String FIND_ALL_AUTHORS_ROUTE = ApiV1Routes.BASE_ROUTE + "/authors";
-    public static final String FIND_AUTHOR_BY_ID_ROUTE = ApiV1Routes.BASE_ROUTE + "/authors/{authorId}";
-    public static final String DELETE_AUTHOR_BY_ID_ROUTE = ApiV1Routes.BASE_ROUTE + "/authors/{authorId}";
+    public static final String RESOURCE = "/authors";
+    public static final String IDENTIFIER = "/{authorId}";
+
+    public static final String CREATE_AUTHOR_ROUTE = ApiV1Routes.BASE_ROUTE + RESOURCE;
+    public static final String FIND_ALL_AUTHORS_ROUTE = ApiV1Routes.BASE_ROUTE + RESOURCE;
+    public static final String FIND_AUTHOR_BY_ID_ROUTE = ApiV1Routes.BASE_ROUTE + RESOURCE + IDENTIFIER;
+    public static final String DELETE_AUTHOR_BY_ID_ROUTE = ApiV1Routes.BASE_ROUTE + RESOURCE + IDENTIFIER;
+    public static final String UPDATE_AUTHOR_BY_ID_ROUTE = ApiV1Routes.BASE_ROUTE + RESOURCE + IDENTIFIER;
 
 }
