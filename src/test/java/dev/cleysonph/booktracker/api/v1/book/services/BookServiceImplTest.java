c0125457ab3eb56f060a143d4c360ecaaba69d40
Cleyson Lima <cleysonph@gmail.com>
2022-08-27 22:42:21 -0300

    create the tests for BookServiceImpl findById

--- src/test/java/dev/cleysonph/booktracker/api/v1/book/services/BookServiceImplTest.java
+++ src/test/java/dev/cleysonph/booktracker/api/v1/book/services/BookServiceImplTest.java
@@ -1,10 +1,11 @@
 package dev.cleysonph.booktracker.api.v1.book.services;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +17,7 @@ import dev.cleysonph.booktracker.api.v1.book.dtos.BookDetailResponse;
 import dev.cleysonph.booktracker.api.v1.book.dtos.BookRequest;
 import dev.cleysonph.booktracker.api.v1.book.dtos.BookSummaryResponse;
 import dev.cleysonph.booktracker.api.v1.book.mappers.BookMapper;
+import dev.cleysonph.booktracker.core.exceptions.BookNotFoundException;
 import dev.cleysonph.booktracker.core.models.Author;
 import dev.cleysonph.booktracker.core.models.Book;
 import dev.cleysonph.booktracker.core.repositories.BookRepository;
@@ -124,6 +126,54 @@ class BookServiceImplTest {
         assertEquals(expectedBooksSummaryResponse.get(0).getIsbn(), actualBooksSummaryResponse.get(0).getIsbn());
         assertEquals(expectedBooksSummaryResponse.get(0).getCoverUrl(), actualBooksSummaryResponse.get(0).getCoverUrl());
         assertEquals(expectedBooksSummaryResponse.get(0).getAuthorId(), actualBooksSummaryResponse.get(0).getAuthorId());
+    }
+
+    @Test
+    void findByIdShouldThrowAnExceptionWhenAnInvalidIdIsGiven() {
+        when(bookRepository.findById(1L)).thenReturn(null);
+        var exception = assertThrows(BookNotFoundException.class, () -> bookService.findById(1L));
+        assertEquals("Book with id 1 not found", exception.getMessage());
+    }
+
+    @Test
+    void findByIdShouldReturnBookDetailResponseWhenAValidIdIsGiven() {
+        var author = Author.builder()
+            .id(1L)
+            .name("Test")
+            .birthDate(LocalDate.of(1996, 1, 1))
+            .deathDate(LocalDate.of(2020, 1, 1))
+            .build();
+        var book = Book.builder()
+            .id(1L)
+            .title("Test")
+            .summary("Test")
+            .pages(100)
+            .isbn("1234567890")
+            .coverUrl("https://example.com")
+            .author(author)
+            .build();
+        var expectedBookDetailResponse = BookDetailResponse.builder()
+            .id(1L)
+            .title("Test")
+            .summary("Test")
+            .pages(100)
+            .isbn("1234567890")
+            .coverUrl("https://example.com")
+            .authorId(1L)
+            .build();
+
+        when(bookRepository.findById(1L)).thenReturn(Optional.of(book));
+        when(bookMapper.toDetailResponse(book)).thenReturn(expectedBookDetailResponse);
+
+        var actualBookDetailResponse = bookService.findById(1L);
+
+        assertEquals(expectedBookDetailResponse.getId(), actualBookDetailResponse.getId());
+        assertEquals(expectedBookDetailResponse.getTitle(), actualBookDetailResponse.getTitle());
+        assertEquals(expectedBookDetailResponse.getSummary(), actualBookDetailResponse.getSummary());
+        assertEquals(expectedBookDetailResponse.getPages(), actualBookDetailResponse.getPages());
+        assertEquals(expectedBookDetailResponse.getIsbn(), actualBookDetailResponse.getIsbn());
+        assertEquals(expectedBookDetailResponse.getCoverUrl(), actualBookDetailResponse.getCoverUrl());
+        assertEquals(expectedBookDetailResponse.getAuthorId(), actualBookDetailResponse.getAuthorId());
     }
 
 }
